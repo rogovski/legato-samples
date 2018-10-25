bbcb8782b3a05879ed96e23eedb3d9d0e9a7e737
Kompletetech <Christian@kompletetech.com>
2018-10-25 16:13:01 +0000

    add header to cdef. print json

--- janssonEcho/janssonEchoServer/server.c
+++ janssonEcho/janssonEchoServer/server.c
@@ -1,9 +1,19 @@
+#include <jansson.h>
 #include "legato.h"
 #include "interfaces.h"
 
 void janssonEchoService_Print(const char* message)
 {
-    LE_INFO("******** Client says '%s'", message);
+	char* s = NULL;
+
+	json_t *root = json_object();
+
+	json_object_set_new( root, "message", json_string(message) );
+
+	s = json_dumps(root, 0);
+
+    LE_INFO(s);
+	json_decref(root);
 }
 
 COMPONENT_INIT
